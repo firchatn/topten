953bece07b1a0ce4fa170c25375443ca4effb98b
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-01 13:23:42 +0100

    v1

--- top/views.py
+++ top/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
+def index(request):
+	with open('/home/firas/git/scripts-python/data.json') as data_file:
+		info = json.load(data_file)
+	return render(request,'top/index.html', {'info' : info})
