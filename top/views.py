f0ea81c9ce4e380c79fc2157bfc0abcfb930aa86
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-01 14:09:59 +0100

    try display items

--- top/views.py
+++ top/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 import json
+from collections import namedtuple
 # Create your views here.
 def index(request):
 	with open('/home/firas/git/scripts-python/data.json') as data_file:
-		info = json.load(data_file)
+		data = json.load(data_file)
+	info = [] 
+	info = data
+
 	return render(request,'top/index.html', {'info' : info})
