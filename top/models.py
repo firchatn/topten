6733c4def51ab7a4f00c4d76b42a698f73f006fd
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-09 14:49:19 +0100

    update video model

--- top/models.py
+++ top/models.py
@@ -6,6 +6,8 @@ class Video(models.Model):
     url = models.CharField(max_length=200)
     vue =  models.IntegerField()
     date = models.CharField(max_length=20)
+    lien = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='upload/',blank=True)
 
     def __str__(self):
         return self.title
