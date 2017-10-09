dd582bd311b73b3d076dcee820ec3e627aba56c1
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-09 12:57:10 +0100

    change data source

--- top/views.py
+++ top/views.py
@@ -3,7 +3,7 @@ import json
 from collections import namedtuple
 # Create your views here.
 def index(request):
-	with open('/home/firas/git/scripts-python/youtube/data.json') as data_file:
+	with open('/home/firas/git/youtubetop/data/data.json') as data_file:
 		data = json.load(data_file)
 	info = [] 
 	info = data[:50] #top 50 video 
