591a42a668ae9ff525ca107b42e555ece038517b
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-09 10:47:50 +0100

    add video model

--- top/models.py
+++ top/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=20)
+    url = models.CharField(max_length=200)
+    vue =  models.IntegerField()
+    date = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
--- top/views.py
+++ top/views.py
@@ -6,5 +6,5 @@ def index(request):
 	with open('/home/firas/git/scripts-python/data.json') as data_file:
 		data = json.load(data_file)
 	info = [] 
-	info = data
+	info = data[:50] #top 50 video 
 	return render(request,'top/index.html', {'info' : info})
